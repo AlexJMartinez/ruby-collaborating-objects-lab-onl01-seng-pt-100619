ec031bd3683bf781ded19b878a85e19adfd2328a
Alex Martinez <alexandr3martin3z@gmail.com>
2019-11-12 03:39:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,7 +18,7 @@ def self.new_by_filename(filename)
   split_file = filename.split(" - ")
   song_name = split_file[1]
   new_song = self.new(song_name)
-  artist_name = split_file[0]
+  #artist_name = split_file[0]
   Artist.all.name = split_file[0]
 
 
